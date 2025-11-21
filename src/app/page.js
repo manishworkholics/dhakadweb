413e594f89672ecd71946544bc2851127e0ab30b
Priyanshi Choudhary <priyanshi.workholics@gmail.com>
2025-11-21 16:39:33 +0530

    code

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import HomePage from "./Home/Page";
-=======
 import HomePage from './Home/Page';
->>>>>>> 3f9187646237b19ed09f9d87792518aef3d947e2
 
 export default function Home() {
   return (
