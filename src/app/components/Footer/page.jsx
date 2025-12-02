c46f6084bfadb0f442ba5e0e0364a3f6e2068e8c
HP <chandan.workholics@gmail.com>
2025-12-02 18:19:40 +0530

    updated

--- src/app/components/Footer/page.jsx
+++ src/app/components/Footer/page.jsx
@@ -8,20 +8,20 @@ export default function Footer() {
             <footer className="bg-white text-dark pt-5 pb-1">
                 <div className="container">
                     <div className="row pb-4">
-                        <div className="col-12 col-md-6 col-lg-2 d-flex align-items-center">
-                            <div className="">
+                        <div className="col-12 col-md-6 col-lg-2 d-flex align-items-center justify-content-center">
+                            <div className="mb-lg-0 mb-md-0 mb-4">
                                 <img src="/dhakadweb/assets/images/dhakadlogo.png" alt="" className="w-100" />
                             </div>
                         </div>
-                        <div className="col-12 col-md-6 col-lg-3">
+                        <div className="col-12 col-md-6 col-lg-3 mb-lg-0 mb-md-0 mb-4 text-center text-lg-start text-md-start">
                             <h6 className="fs-6 fw-semibold text-dark mb-2 text-uppercase">Address</h6>
-                            <address className="text-sm text-secondary">
+                            <div className="text-sm text-secondary address">
                                 <p className="mb-0 text-5A5A5A">315, Princes Business Skypark, AB Road, Vijay Nagar Indore, M.P. 452010</p>
-                            </address>
+                            </div>
                         </div>
-                        <div className="col-12 col-md-6 col-lg-3">
+                        <div className="col-12 col-md-6 col-lg-3 mb-lg-0 mb-md-0 mb-4 text-center text-lg-start text-md-start">
                             <h6 className="fs-6 fw-semibold text-dark mb-2 text-uppercase">Contact</h6>
-                            <div className="text-icon d-flex">
+                            <div className="text-icon d-flex justify-content-center justify-content-lg-start justify-content-md-start">
                                 <div className="icon me-2">
                                     <i className="fa-solid fa-phone text-danger"></i>
                                 </div>
@@ -29,7 +29,7 @@ export default function Footer() {
                                     <p className="mb-1 text-5A5A5A fw-medium">0731-4979600</p>
                                 </div>
                             </div>
-                            <div className="text-icon d-flex">
+                            <div className="text-icon d-flex justify-content-center justify-content-lg-start justify-content-md-start">
                                 <div className="icon me-2">
                                     <i className="fa-solid fa-phone text-danger"></i>
                                 </div>
@@ -37,7 +37,7 @@ export default function Footer() {
                                     <p className="mb-1 text-5A5A5A fw-medium">8982079600, 8770896005-6-7</p>
                                 </div>
                             </div>
-                            <div className="text-icon d-flex">
+                            <div className="text-icon d-flex justify-content-center justify-content-lg-start justify-content-md-start">
                                 <div className="icon me-2">
                                     <i className="fa-regular fa-envelope text-danger"></i>
                                 </div>
@@ -46,19 +46,19 @@ export default function Footer() {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-12 col-md-6 col-lg-2 d-flex align-items-center">
-                            <img src="/dhakadweb/assets/images/sure 1.png" alt="" className="w-100" />
+                        <div className="col-12 col-md-6 col-lg-2 d-flex align-items-center justify-content-lg-start justify-content-md-start justify-content-center mb-lg-0 mb-md-0 mb-4">
+                            <img src="/dhakadweb/assets/images/sure 1.png" alt="" className="w-lg-100 w-md-100 w-50" />
                         </div>
                         <div className="col-12 col-md-6 col-lg-2 d-flex align-items-center">
-                            <div className="social d-flex justify-content-between w-100">
+                            <div className="social d-flex justify-content-lg-between justify-content-md-between justify-content-center w-100">
                                 <div className="icon border border-1 rounded-circle px-2 py-1">
                                     <Link href="">
                                         <i className="fa-brands fa-facebook text-secondary"></i></Link>
                                 </div>
-                                <div className="icon border border-1 rounded-circle px-2 py-1">
+                                <div className="icon border border-1 rounded-circle px-2 py-1 mx-lg-0 mx-md-0 mx-2">
                                     <Link href=""> <i className="fa-brands fa-x-twitter text-secondary"></i></Link>
                                 </div>
-                                <div className="icon border border-1 rounded-circle px-2 py-1 me-1">
+                                <div className="icon border border-1 rounded-circle px-2 py-1 me-lg-1 me-2 me-md-1">
                                     <Link href=""><i className="fa-brands fa-instagram text-secondary"></i></Link>
                                 </div>
                                 <div className="icon border border-1 rounded-circle px-2 py-1">
@@ -70,9 +70,15 @@ export default function Footer() {
                 </div>
             </footer>
             <div className="container-fluid bg-EBEBEB py-2">
-                <div className="container d-flex justify-content-between align-items-center">
-                    <p className="mb-0 fw-normal text-787878" style={{ fontSize: "14px" }}>Copyright 2020. All Rights Reserved</p>
-                    <p className="mb-0 fw-normal text-787878" style={{ fontSize: "14px" }}>Term & conditions     |    Privacy Policy</p>
+                <div className="container">
+                    <div className="row justify-content-between">
+                        <div className="col-lg-6 col-md-6 col-12">
+                            <p className="mb-0 fw-normal text-787878 text-center text-lg-start text-md-start" style={{ fontSize: "14px" }}>Copyright 2020. All Rights Reserved</p>
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-12">
+                            <p className="mb-0 fw-normal text-787878 text-center text-lg-end text-md-end" style={{ fontSize: "14px" }}>Term & conditions     |    Privacy Policy</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
